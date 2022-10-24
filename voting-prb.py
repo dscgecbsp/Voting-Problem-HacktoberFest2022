14672dacc23ce161f12d9e5b3742467b2788b335
ebinalexurk21cs2044 <ebinalex@karunya.edu.in>
2022-10-24 13:08:26 +0530

    voting prob completed

--- voting-prb.py
+++ voting-prb.py
@@ -1,2 +1,8 @@
-class voting:
-    def 
+User_name = input("Enter your NAME: ")  #Name of user
+User_age = int(input("Enter your AGE: "))  #Age of user
+
+if(User_age>18): #age checking
+    print(f"{User_name} is eligible to vote")
+else:
+    print(f"{User_name} is not eligible to vote")
+
